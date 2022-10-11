8d91c4152c970b28075aceecfe3fcaf5f47b97d7
Agatha Schneider <agatha.schneider@outlook.com>
2022-10-11 11:37:51 -0300

    quiz 3

--- exercises/traits/traits3.rs
+++ exercises/traits/traits3.rs
@@ -7,10 +7,11 @@
 // Consider what you can add to the Licensed trait.
 // Execute `rustlings hint traits3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub trait Licensed {
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String {
+        "Some information".to_string()
+    }
 }
 
 struct SomeSoftware {
@@ -31,10 +32,10 @@ mod tests {
     #[test]
     fn is_licensing_info_the_same() {
         let licensing_info = String::from("Some information");
+
         let some_software = SomeSoftware { version_number: 1 };
-        let other_software = OtherSoftware {
-            version_number: "v2.0.0".to_string(),
-        };
+        let other_software = OtherSoftware { version_number: "v2.0.0".to_string() };
+
         assert_eq!(some_software.licensing_info(), licensing_info);
         assert_eq!(other_software.licensing_info(), licensing_info);
     }
